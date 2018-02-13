2f5dd75a67d231fe8fc7414c2ebe7b4f66350370
Adrian Militante <adriangpc@gmail.com>
2018-02-13 18:31:58 +0800

    End of 22

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -132,16 +132,16 @@ class RestaurantDetailView(DetailView):
     queryset = RestaurantLocation.objects.all()
     #template_name = 'restaurants/restaurant_list.html'
 
-    def get_context_data(self, *args, **kwargs):
-        print(self.kwargs)
-        context = super(RestaurantDetailView, self).get_context_data(*args, **kwargs)
-        print(context)
-        return context
+    # def get_context_data(self, *args, **kwargs):
+    #     print(self.kwargs)
+    #     context = super(RestaurantDetailView, self).get_context_data(*args, **kwargs)
+    #     print(context)
+    #     return context
 
-    def get_object(self, *args, **kwargs):
-        rest_id = self.kwargs.get('rest_id')
-        obj = get_object_or_404(RestaurantLocation, id=rest_id)
-        return obj
+    # def get_object(self, *args, **kwargs):
+    #     rest_id = self.kwargs.get('rest_id')
+    #     obj = get_object_or_404(RestaurantLocation, id=rest_id)
+    #     return obj
 
 
 
